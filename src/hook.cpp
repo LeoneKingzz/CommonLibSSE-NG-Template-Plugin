c8164c8a54a17877525fbb580810b633e51e6a88
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-05-19 17:26:11 +0100

    one

--- src/hook.cpp
+++ src/hook.cpp
@@ -19,11 +19,32 @@ public:
     RE::BSEventNotifyControl ProcessEvent(const RE::TESCombatEvent* event, RE::BSTEventSource<RE::TESCombatEvent>*) {
         auto targetName = event->targetActor;
         auto sourceName = event->actor;
+        auto Dragon = sourceName->As<RE::Actor>();
+        const auto ActorTypeDragon = RE::BGSKeyword::LookupByEditorID("ActorTypeDragon")->As<RE::BGSKeyword>();
+        auto UDDragonAISpell = RE::TESForm::LookupByEditorID<RE::SpellItem>("aaaUDDragonAISpell");
+
+        bool IsActorTypeDragon = Dragon->HasKeyword(ActorTypeDragon);
+        bool HasUDDragonAISpell = Dragon->HasSpell(UDDragonAISpell);
+
+        if (!IsActorTypeDragon) {
+            RE::BSEventNotifyControl::kContinue;
+        }
+        if (!HasUDDragonAISpell) {
+            RE::BSEventNotifyControl::kContinue;
+        }
         
-        return RE::BSEventNotifyControl::kContinue;
+        auto UDQuest = RE::TESForm::LookupByEditorID<RE::TESQuest>("aaaUDInitQuest");
+
+        if (UDQuest) {
+
+            if (event->newState == RE::ACTOR_COMBAT_STATE::kCombat && !Dragon->IsBeingRidden())
+            {
+                Dragon->AddSpell(UDDragonAISpell);
+            }
+        }
+        RE::BSEventNotifyControl::kContinue;
+
     }
-
-
 };
 
 void IHooks::install() {
@@ -54,6 +75,48 @@ void IHooks::install() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// auto vmhandle = RE::SkyrimVM::GetSingleton()->handlePolicy.GetHandleForObject(77, UDQuest);
+
+// UDQuest
+// RE::BSScript::IObjectHandlePolicy::GetHandleForObject(77, UDQuest);
+// RE::VMHandle vmhandle;
+// return RE::BSEventNotifyControl::kContinue;
+// RE::VMHandle* vmhandle = RE::BSScript::IObjectHandlePolicy::GetHandleForObject(77, UDQuest);
+// event->newState
+
+// std::string* a = "UD_OnCombat"
+// static inline const char* s = "UD_OnCombat";
+// RE::BSFixedString eventName = s;
+// event->newState == RE::ACTOR_COMBAT_STATE::kCombat &&  Dragon->IsBeingRidden() ? Dragon->AddSpell(UDDragonAISpell)
+// RE::BSScript::Internal::VirtualMachine::GetSingleton()->DispatchMethodCall2(vmhandle, nullptr, eventName,(Dragon),)
+// RE::BSScript::IFunctionArguments::operator()<Dragon, event->newState>
+// RE::SkyrimVM::GetSingleton()->RelayEvent(vmhandle, &eventName, <Dragon>,)
 
 // auto targetName = event->target->GetBaseObject()->GetName();
 // auto sourceName = event->cause->GetBaseObject()->GetName();
